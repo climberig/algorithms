fdf083bc6cdb69c0ce5382ad9e6441b3ae60ef3b
igor <climberig@gmail.com>
2021-11-07 07:49:54 -0800

    git ignore

--- src/leetcode/a.java
+++ src/leetcode/a.java
@@ -1,19 +1,15 @@
 package leetcode;
 
 public class a {
-    public int numRookCaptures(char[][] b) {
-        for (int i = 0; i < b.length; i++)
-            for (int j = 0; j < b.length; j++)
-                if (b[i][j] == 'R')
-                    return cap(1, 0, i, j, b) + cap(-1, 0, i, j, b) + cap(0, 1, i, j, b) + cap(0, -1, i, j, b);
-        return 0;
+    public static void main(String[] args) {
+        System.out.println(new a().countVowels("igor"));
     }
-
-    int cap(int di, int dj, int i, int j, char[][] b) {
-        for (i = i + di, j = j + dj; 0 <= i && i < b.length && 0 <= j && j < b.length && b[i][j] != 'B'; i += di, j += dj)
-            if (b[i][j] == 'p')
-                return 1;
-        return 0;
+    public long countVowels(String word) {
+        long r = 0;
+        for (int i = 0; i < word.length(); i++)
+            if ("aeiou".indexOf(word.charAt(i)) >= 0)
+                r += (i + 1L) * (word.length() - i);
+        return r;
     }
 }
 
--- src/leetcode/p2000.java
+++ src/leetcode/p2000.java
@@ -688,4 +688,22 @@ public class p2000 {
             return -1;
         }
     }
+
+    static class s2062 {
+        public int countVowelSubstrings(String word) {//Count Vowel Substrings of a String
+            Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u'), curr = new HashSet<>();
+            int r = 0;
+            char[] w = word.toCharArray();
+            for (int i = 0; i < w.length; i++, curr.clear())
+                for (int j = i; j < w.length && vowels.contains(w[j]); j++) {
+                    curr.add(w[j]);
+                    r += curr.size() == vowels.size() ? 1 : 0;
+                }
+            return r;
+        }
+    }
+
+    static class s2063{
+
+    }
 }
