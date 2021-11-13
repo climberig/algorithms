80c63c9f2f2b0adbfbd7f97eb6fe2cfda2c7fec0
igor <climberig@gmail.com>
2021-11-12 20:35:35 -0800

    solutions

--- src/leetcode/a.java
+++ src/leetcode/a.java
@@ -1,23 +1,12 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-public class a {
-    public String[] findRestaurant(String[] list1, String[] list2) {
-        int minSum = Integer.MAX_VALUE;
-        Map<String, Integer> list1m = IntStream.range(0, list1.length).boxed().collect(Collectors.toMap(i -> list1[i], i -> i));
-        Map<Integer, List<String>> r = new HashMap<>();
-        for (int i = 0; i < list2.length; i++)
-            if (list1m.containsKey(list2[i])) {
-                int sum = list1m.get(list2[i]) + i;
-                r.computeIfAbsent(sum, l -> new ArrayList<>()).add(list2[i]);
-                minSum = Math.min(minSum, sum);
-            }
-        return r.get(minSum).toArray(new String[0]);
+public class a{
+    public boolean isLongPressedName(String name, String typed){
+        int i = 0, j = 0;
+        while(i < name.length() && j < typed.length() && name.charAt(i) == typed.charAt(j)){
+            for(; i < name.length() && j < typed.length() && name.charAt(i) == typed.charAt(j); i++, j++) ;
+            for(; j < typed.length() && name.charAt(i - 1) == typed.charAt(j); j++) ;
+        }
+        return i == name.length() && j == typed.length();
     }
 }
--- src/leetcode/p900.java
+++ src/leetcode/p900.java
@@ -1,40 +1,50 @@
 package leetcode;
 
 import java.util.*;
-import java.util.stream.*;
+import java.util.stream.IntStream;
 
-public class p900 {
-    static class s927 {//Three Equal Parts
-        public int[] threeEqualParts(int[] a) {
+public class p900{
+    static class s925{
+        public boolean isLongPressedName(String name, String typed){
+            int i = 0, j = 0;
+            while(i < name.length() && j < typed.length() && name.charAt(i) == typed.charAt(j)){
+                for(; i < name.length() && j < typed.length() && name.charAt(i) == typed.charAt(j); i++, j++) ;
+                for(; j < typed.length() && name.charAt(i - 1) == typed.charAt(j); j++) ;
+            }
+            return i == name.length() && j == typed.length();
+        }
+    }
+    static class s927{//Three Equal Parts
+        public int[] threeEqualParts(int[] a){
             int ones = Arrays.stream(a).sum(), negative[] = {-1, -1};
-            if (ones == 0)
+            if(ones == 0)
                 return new int[]{0, 2};
-            if (ones % 3 != 0)
+            if(ones % 3 != 0)
                 return negative;
             int third = a.length - 1;
-            for (int count = ones / 3; count > 0; count -= a[third--]) ;
+            for(int count = ones / 3; count > 0; count -= a[third--]) ;
             int i = find(a, 0, third + 1), j = find(a, i, third + 1);
             return i >= 0 && j >= 0 ? new int[]{i - 1, j} : negative;
         }
 
-        int find(int[] a, int i, int j) {
-            if (i == -1)
+        int find(int[] a, int i, int j){
+            if(i == -1)
                 return -1;
-            for (; a[i] == 0; i++) ;
-            for (; j < a.length; j++, i++)
-                if (a[i] != a[j])
+            for(; a[i] == 0; i++) ;
+            for(; j < a.length; j++, i++)
+                if(a[i] != a[j])
                     return -1;
             return i;
         }
     }
 
-    static class s935 {//Knight Dialer
-        public int knightDialer(int n) {
+    static class s935{//Knight Dialer
+        public int knightDialer(int n){
             int m[][] = {{4, 6}, {6, 8}, {7, 9}, {4, 8}, {0, 3, 9}, {}, {0, 1, 7}, {2, 6}, {1, 3}, {2, 4}}, counts[] = new int[10];
-            for (Arrays.fill(counts, 1); n > 1; n--) {
+            for(Arrays.fill(counts, 1); n > 1; n--){
                 int[] nextCounts = new int[10];
-                for (int i = 0; i <= 9; i++)
-                    for (int d : m[i])
+                for(int i = 0; i <= 9; i++)
+                    for(int d : m[i])
                         nextCounts[d] = (counts[i] + nextCounts[d]) % 1_000_000_007;
                 counts = nextCounts;
             }
@@ -42,31 +52,31 @@ public class p900 {
         }
     }
 
-    static class s947 {//Most Stones Removed with Same Row or Column
-        public int removeStones(int[][] stones) {
+    static class s947{//Most Stones Removed with Same Row or Column
+        public int removeStones(int[][] stones){
             int[] uf = IntStream.range(0, stones.length).toArray();
-            for (int i = 0; i < stones.length; i++)
-                for (int j = i + 1; j < stones.length; j++)
-                    if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+            for(int i = 0; i < stones.length; i++)
+                for(int j = i + 1; j < stones.length; j++)
+                    if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
                         int x = find(i, uf), y = find(j, uf);
-                        if (x != y)
+                        if(x != y)
                             uf[x] = y;
                     }
             return stones.length - (int) IntStream.range(0, stones.length).filter(x -> x == find(x, uf)).count();
         }
 
-        int find(int x, int[] uf) {return x == uf[x] ? x : find(uf[x], uf);}
+        int find(int x, int[] uf){return x == uf[x] ? x : find(uf[x], uf);}
     }
 
-    static class s948 {//Bag of Tokens
-        public int bagOfTokensScore(int[] tokens, int power) {
+    static class s948{//Bag of Tokens
+        public int bagOfTokensScore(int[] tokens, int power){
             Arrays.sort(tokens);
             int r = 0, score = 0;
-            for (int i = 0, j = tokens.length - 1; i <= j && score >= 0; )
-                if (power >= tokens[i]) {
+            for(int i = 0, j = tokens.length - 1; i <= j && score >= 0; )
+                if(power >= tokens[i]){
                     power -= tokens[i++];
                     r = Math.max(r, ++score);
-                } else {
+                }else{
                     power += tokens[j--];
                     score--;
                 }
@@ -74,20 +84,20 @@ public class p900 {
         }
     }
 
-    static class s952 {//Largest Component Size by Common Factor
-        public int largestComponentSize(int[] a) {
+    static class s952{//Largest Component Size by Common Factor
+        public int largestComponentSize(int[] a){
             int[] uf = IntStream.range(0, 100_001).toArray();
-            for (int n : a)
-                for (int d = 2; d <= Math.sqrt(n); d++)
-                    if (n % d == 0)
-                        for (Integer q : Arrays.asList(d, n / d)) {
+            for(int n : a)
+                for(int d = 2; d <= Math.sqrt(n); d++)
+                    if(n % d == 0)
+                        for(Integer q : Arrays.asList(d, n / d)){
                             int x = find(n, uf), y = find(q, uf);
-                            if (x != y)
+                            if(x != y)
                                 uf[x] = y;
                         }
             Map<Integer, Integer> m = new HashMap<>();
             int r = 0;
-            for (int n : a) {
+            for(int n : a){
                 int j = find(n, uf);
                 m.put(j, m.getOrDefault(j, 0) + 1);
                 r = Math.max(r, m.get(j));
@@ -95,19 +105,19 @@ public class p900 {
             return r;
         }
 
-        int find(int x, int[] uf) {
+        int find(int x, int[] uf){
             return x == uf[x] ? x : (uf[x] = find(uf[x], uf));
         }
     }
 
-    static class s954 {//Array of Doubled Pairs
-        public boolean canReorderDoubled(int[] a) {
+    static class s954{//Array of Doubled Pairs
+        public boolean canReorderDoubled(int[] a){
             Map<Integer, Integer> m = new TreeMap<>();
             Arrays.stream(a).forEach(n -> m.put(n, m.getOrDefault(n, 0) + 1));
-            for (int x : m.keySet())
-                if (m.get(x) > 0) {
+            for(int x : m.keySet())
+                if(m.get(x) > 0){
                     int x2 = x < 0 ? x / 2 : x * 2;
-                    if (x < 0 && x % 2 != 0 || m.get(x) > m.getOrDefault(x2, 0))
+                    if(x < 0 && x % 2 != 0 || m.get(x) > m.getOrDefault(x2, 0))
                         return false;
                     m.put(x2, m.get(x2) - m.get(x));
                 }
@@ -115,14 +125,14 @@ public class p900 {
         }
     }
 
-    static class s955 {//Delete Columns to Make Sorted II
-        public int minDeletionSize(String[] ss) {
+    static class s955{//Delete Columns to Make Sorted II
+        public int minDeletionSize(String[] ss){
             Set<Integer> deleteCols = new HashSet<>();
-            for (int row = 1; row < ss.length; row++)
-                for (int col = 0; col < ss[row].length(); col++) {
-                    if (deleteCols.contains(col) || ss[row - 1].charAt(col) == ss[row].charAt(col))
+            for(int row = 1; row < ss.length; row++)
+                for(int col = 0; col < ss[row].length(); col++){
+                    if(deleteCols.contains(col) || ss[row - 1].charAt(col) == ss[row].charAt(col))
                         continue;
-                    if (ss[row - 1].charAt(col) > ss[row].charAt(col)) {
+                    if(ss[row - 1].charAt(col) > ss[row].charAt(col)){
                         deleteCols.add(col);
                         row = 0;
                     }
@@ -132,16 +142,16 @@ public class p900 {
         }
     }
 
-    static class s968 {//Binary Tree Cameras
+    static class s968{//Binary Tree Cameras
         int r, leaf = 0, covered = 1, leaf_parent = 2;
 
-        public int minCameraCover(TreeNode root) {return (dfs(root) == leaf ? 1 : 0) + r;}
+        public int minCameraCover(TreeNode root){return (dfs(root) == leaf ? 1 : 0) + r;}
 
-        int dfs(TreeNode node) {
-            if (node == null)
+        int dfs(TreeNode node){
+            if(node == null)
                 return covered;
             int left = dfs(node.left), right = dfs(node.right);
-            if (left == leaf || right == leaf) {
+            if(left == leaf || right == leaf){
                 r++;
                 return leaf_parent;
             }
@@ -149,43 +159,43 @@ public class p900 {
         }
     }
 
-    static class s982 {//Triples with Bitwise AND Equal To Zero
-        public int countTriplets(int[] a) {
+    static class s982{//Triples with Bitwise AND Equal To Zero
+        public int countTriplets(int[] a){
             int m[] = new int[1 << 16], r = 0;
-            for (int i : a)
-                for (int j : a)
+            for(int i : a)
+                for(int j : a)
                     m[i & j]++;
-            for (int i : a)
-                for (int j = 0; j < m.length; j++)
-                    if ((i & j) == 0)
+            for(int i : a)
+                for(int j = 0; j < m.length; j++)
+                    if((i & j) == 0)
                         r += m[j];
             return r;
         }
     }
 
-    static class s998 {//Maximum Binary Tree II
-        public TreeNode insertIntoMaxTree(TreeNode root, int val) {
-            if (root == null)
+    static class s998{//Maximum Binary Tree II
+        public TreeNode insertIntoMaxTree(TreeNode root, int val){
+            if(root == null)
                 return new TreeNode(val);
-            if (root.val < val)
+            if(root.val < val)
                 return new TreeNode(val, root, null);
             root.right = insertIntoMaxTree(root.right, val);
             return root;
         }
     }
 
-    static class s999 {//Available Captures for Rook
-        public int numRookCaptures(char[][] b) {
-            for (int i = 0; i < b.length; i++)
-                for (int j = 0; j < b.length; j++)
-                    if (b[i][j] == 'R')
+    static class s999{//Available Captures for Rook
+        public int numRookCaptures(char[][] b){
+            for(int i = 0; i < b.length; i++)
+                for(int j = 0; j < b.length; j++)
+                    if(b[i][j] == 'R')
                         return cap(1, 0, i, j, b) + cap(-1, 0, i, j, b) + cap(0, 1, i, j, b) + cap(0, -1, i, j, b);
             return 0;
         }
 
-        int cap(int di, int dj, int i, int j, char[][] b) {
-            for (i = i + di, j = j + dj; 0 <= i && i < b.length && 0 <= j && j < b.length && b[i][j] != 'B'; i += di, j += dj)
-                if (b[i][j] == 'p')
+        int cap(int di, int dj, int i, int j, char[][] b){
+            for(i = i + di, j = j + dj; 0 <= i && i < b.length && 0 <= j && j < b.length && b[i][j] != 'B'; i += di, j += dj)
+                if(b[i][j] == 'p')
                     return 1;
             return 0;
         }
