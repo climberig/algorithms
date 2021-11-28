318e794c178d23dc85cbaef77ae9175924fd8734
igor <climberig@gmail.com>
2021-11-27 20:11:33 -0800

    new solutions

--- src/leetcode/a.java
+++ src/leetcode/a.java
@@ -1,14 +1,13 @@
 package leetcode;
 
 public class a{
-    static class s2087{//Minimum Cost Homecoming of a Robot in a Grid
-        public int minCost(int[] start, int[] home, int[] rowCosts, int[] colCosts){
-            int cost = 0;
-            for(int r = start[0] + 1; r <= home[0]; cost += rowCosts[r++]) ;
-            for(int c = start[1] + 1; c <= home[1]; cost += colCosts[c++]) ;
-            for(int r = start[0] - 1; r >= home[0]; cost += rowCosts[r--]) ;
-            for(int c = start[1] - 1; c >= home[1]; cost += colCosts[c--]) ;
-            return cost;
-        }
+    public static void main(String[] args){
+        System.out.println(new a().minCost(new int[]{1, 0}, new int[]{2, 3}, new int[]{5, 4, 3}, new int[]{8, 2, 6, 7}));
+    }
+    public int minCost(int[] start, int[] home, int[] rowCosts, int[] colCosts){
+        int cost = 0, rStep = (int) Math.signum(home[0] - start[0]), cStep = (int) Math.signum(home[1] - start[1]);
+        for(int r = start[0] + rStep; r != home[0] + rStep; cost += rowCosts[r], r += rStep) ;
+        for(int c = start[1] + cStep; c != home[1] + cStep; cost += colCosts[c], c += cStep) ;
+        return cost;
     }
 }
--- src/leetcode/p2000.java
+++ src/leetcode/p2000.java
@@ -941,13 +941,10 @@ public class p2000{
 
     static class s2087{//Minimum Cost Homecoming of a Robot in a Grid
         public int minCost(int[] start, int[] home, int[] rowCosts, int[] colCosts){
-            int cost = 0;
-            for(int r = start[0] + 1; r <= home[0]; cost += rowCosts[r++]) ;
-            for(int c = start[1] + 1; c <= home[1]; cost += colCosts[c++]) ;
-            for(int r = start[0] - 1; r >= home[0]; cost += rowCosts[r--]) ;
-            for(int c = start[1] - 1; c >= home[1]; cost += colCosts[c--]) ;
+            int cost = 0, rStep = (int) Math.signum(home[0] - start[0]), cStep = (int) Math.signum(home[1] - start[1]);
+            for(int r = start[0] + rStep; r != home[0] + rStep; cost += rowCosts[r], r += rStep) ;
+            for(int c = start[1] + cStep; c != home[1] + cStep; cost += colCosts[c], c += cStep) ;
             return cost;
         }
     }
-
 }
