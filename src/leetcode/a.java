c0b61fca44f787f17e00f193d6db1dde950f460f
igor <climberig@gmail.com>
2021-11-28 19:17:17 -0800

    new solutions

--- src/leetcode/a.java
+++ src/leetcode/a.java
@@ -1,20 +1,27 @@
 package leetcode;
 
+import java.util.Arrays;
 public class a{
-    static class s2091{//Removing Minimum and Maximum From Array
-        public int minimumDeletions(int[] a){
-            int minIdx = 0, maxIdx = 0;
-            for(int i = 1; i < a.length; i++){
-                if(a[minIdx] > a[i])
-                    minIdx = i;
-                if(a[maxIdx] < a[i])
-                    maxIdx = i;
-            }
-            int left = Math.min(minIdx, maxIdx), right = Math.max(minIdx, maxIdx);
-            int cutBoth = left + 1 + a.length - right;
-            int cutLeft = 1 + right;
-            int cutRight = a.length - left;
-            return Math.min(cutBoth, Math.min(cutLeft, cutRight));
+    static class s2088{//Count Fertile Pyramids in a Land
+        public int countPyramids(int[][] g){
+            return count(inverse(g)) + count(g);
+        }
+
+        int count(int[][] g){
+            int r = 0;
+            for(int i = 1; i < g.length; i++)
+                for(int j = 1; j < g[0].length - 1; j++)
+                    if(g[i][j] >= 1 && g[i - 1][j] >= 1){
+                        g[i][j] = Math.min(g[i - 1][j - 1], g[i - 1][j + 1]) + 1;
+                        r += g[i][j] - 1;
+                    }
+            return r;
+        }
+        int[][] inverse(int[][] g){
+            int[][] copy = new int[g.length][g[0].length];
+            for(int i = 0; i < g.length; i++)
+                copy[g.length - i - 1] = Arrays.copyOf(g[i], g[i].length);
+            return copy;
         }
     }
 }
--- src/leetcode/p2000.java
+++ src/leetcode/p2000.java
@@ -948,6 +948,29 @@ public class p2000{
         }
     }
 
+    static class s2088{//Count Fertile Pyramids in a Land
+        public int countPyramids(int[][] g){
+            return count(inverse(g)) + count(g);
+        }
+
+        int count(int[][] g){
+            int r = 0;
+            for(int i = 1; i < g.length; i++)
+                for(int j = 1; j < g[0].length - 1; j++)
+                    if(g[i][j] >= 1 && g[i - 1][j] >= 1){
+                        g[i][j] = Math.min(g[i - 1][j - 1], g[i - 1][j + 1]) + 1;
+                        r += g[i][j] - 1;
+                    }
+            return r;
+        }
+        int[][] inverse(int[][] g){
+            int[][] copy = new int[g.length][g[0].length];
+            for(int i = 0; i < g.length; i++)
+                copy[g.length - i - 1] = Arrays.copyOf(g[i], g[i].length);
+            return copy;
+        }
+    }
+
     static class s2090{//K Radius Subarray Averages
         public int[] getAverages(int[] a, int k){
             int[] r = new int[a.length];
