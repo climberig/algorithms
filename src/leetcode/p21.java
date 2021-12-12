9a32a16e5e5063de7b4a8eef76a8cd6343f8af4a
igor <climberig@gmail.com>
2021-12-11 20:04:18 -0800

    new solutions and renamed solution containers

--- src/leetcode/p21.java
+++ src/leetcode/p21.java
@@ -2,6 +2,8 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class p21{
     static class s2100{//Find Good Days to Rob the Bank
@@ -16,6 +18,30 @@ public class p21{
             if(time == 0)
                 r.addAll(Arrays.asList(0, sec.length - 1));
             return r;
+        }
+    }
+
+    static class s2101{//Detonate the Maximum Bombs
+        public int maximumDetonation(int[][] bombs){
+            List<List<Integer>> g = IntStream.range(0, bombs.length).mapToObj(i -> new ArrayList<Integer>()).collect(Collectors.toList());
+            for(int i = 0; i < bombs.length; i++)
+                for(int j = 0; j < bombs.length; j++)
+                    if(i != j && detonates(i, j, bombs))
+                        g.get(i).add(j);
+            int r = 0;
+            for(int i = 0; i < bombs.length; i++)
+                r = Math.max(dfs(i, g, new boolean[bombs.length]), r);
+            return r;
+        }
+
+        int dfs(int u, List<List<Integer>> g, boolean[] seen){
+            seen[u] = true;
+            return 1 + g.get(u).stream().filter(v -> !seen[v]).mapToInt(v -> dfs(v, g, seen)).sum();
+        }
+
+        boolean detonates(int i, int j, int[][] bombs){//true if i bomb detonates j bomb
+            long x = bombs[j][0] - bombs[i][0], y = bombs[j][1] - bombs[i][1];
+            return x * x + y * y <= (long) bombs[i][2] * bombs[i][2];
         }
     }
 }
