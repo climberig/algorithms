a5b7b19c825371698a05fe49ace6ac280c75701c
igor <climberig@gmail.com>
2021-12-14 15:49:38 -0800

    new solution

--- src/leetcode/p20.java
+++ src/leetcode/p20.java
@@ -1193,6 +1193,31 @@ public class p20{
         }
     }
 
+    static class s2097{//Valid Arrangement of Pairs
+        public int[][] validArrangement(int[][] pairs){
+            Map<Integer, Stack<Integer>> g = new HashMap<>();
+            Map<Integer, Integer> out = new HashMap<>();
+            for(int[] p : pairs){
+                g.putIfAbsent(p[0], new Stack<>());
+                g.get(p[0]).add(p[1]);
+                out.put(p[0], out.getOrDefault(p[0], 0) + 1);
+                out.put(p[1], out.getOrDefault(p[1], 0) - 1);
+            }
+            int start = out.keySet().stream().filter(k -> out.get(k) == 1).findFirst().orElse(pairs[0][0]);
+            LinkedList<int[]> r = new LinkedList<>();
+            arrange(start, g, r);
+            return r.toArray(new int[0][]);
+        }
+
+        void arrange(int u, Map<Integer, Stack<Integer>> g, LinkedList<int[]> r){
+            for(Stack<Integer> s = g.getOrDefault(u, null); s != null && !s.isEmpty(); ){
+                Integer v = s.pop();
+                arrange(v, g, r);
+                r.addFirst(new int[]{u, v});
+            }
+        }
+    }
+
     static class s2098{//Subsequence of Size K With the Largest Even Sum
         public long largestEvenSum(int[] a, int k){
             Arrays.sort(a);
