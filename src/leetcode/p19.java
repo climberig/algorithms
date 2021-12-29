f2ea87068b9b47666b262b4295f938aed32f4927
igor <climberig@gmail.com>
2021-12-28 18:09:45 -0800

    new solutions

--- src/leetcode/p19.java
+++ src/leetcode/p19.java
@@ -67,6 +67,27 @@ public class p19{
         }
     }
 
+    static class s1937{//Maximum Number of Points with Cost
+        public long maxPoints(int[][] points){
+            int n = points[0].length;
+            long[] pre = new long[n];
+            for(int c = 0; c < n; c++)
+                pre[c] = points[0][c];
+            for(int r = 1; r < points.length; r++){
+                long[] left = new long[n], right = new long[n];
+                left[0] = pre[0];
+                for(int c = 1; c < n; c++)
+                    left[c] = Math.max(left[c - 1] - 1, pre[c]);
+                right[n - 1] = pre[n - 1];
+                for(int c = n - 2; c >= 0; c--)
+                    right[c] = Math.max(right[c + 1] - 1, pre[c]);
+                for(int c = 0; c < n; c++)
+                    pre[c] = Math.max(left[c], right[c]) + points[r][c];
+            }
+            return Arrays.stream(pre).max().getAsLong();
+        }
+    }
+
     static class s1941{//Check if All Characters Have Equal Number of Occurrences
         public boolean areOccurrencesEqual(String s){
             int[] fr = new int[26];
