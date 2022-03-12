0dc07f9e24e6cf6cd9ba9c41f515b288fb20dd54
igor <climberig@gmail.com>
2022-03-11 18:05:32 -0800

    new solutions

--- src/leetcode/p7.java
+++ src/leetcode/p7.java
@@ -124,6 +124,14 @@ public class p7{
         }
     }
 
+    static class s746{//Min Cost Climbing Stairs
+        public int minCostClimbingStairs(int[] cost){
+            for(int i = 2; i < cost.length; i++)
+                cost[i] += Math.min(cost[i - 2], cost[i - 1]);
+            return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+        }
+    }
+
     static class s749{//Contain Virus
         public int containVirus(int[][] g){
             int[] dirs = {-1, 0, 1, 0, -1};
