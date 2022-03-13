058626c451457680916e6fea40cc4597ffdfc846
igor <climberig@gmail.com>
2022-03-12 20:15:53 -0800

    new solutions

--- src/leetcode/p22.java
+++ src/leetcode/p22.java
@@ -2,8 +2,6 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.List;
 public class p22{
-
-
     static class s2200{
         public List<Integer> findKDistantIndices(int[] a, int key, int k){
             List<Integer> r = new ArrayList<>();
