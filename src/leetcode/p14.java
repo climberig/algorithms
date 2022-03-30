1a587a6e8d0a6e49b07a0dbc30b579833661e327
igor <climberig@gmail.com>
2022-03-30 10:22:19 -0700

    new solutions

--- src/leetcode/p14.java
+++ src/leetcode/p14.java
@@ -150,6 +150,23 @@ public class p14{
         }
     }
 
+    static class s1458{//Max Dot Product of Two Subsequences
+        public int maxDotProduct(int[] a, int[] b){
+            int n = a.length, m = b.length, dp[][] = new int[n][m];
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < m; j++){
+                    dp[i][j] = a[i] * b[j];
+                    if(i > 0 && j > 0)
+                        dp[i][j] += Math.max(dp[i - 1][j - 1], 0);
+                    if(i > 0)
+                        dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+                    if(j > 0)
+                        dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
+                }
+            return dp[n - 1][m - 1];
+        }
+    }
+
     static class s1462{//Course Schedule IV
         public List<Boolean> checkIfPrerequisite(int n, int[][] prerequisites, int[][] queries){
             List<List<Integer>> g = IntStream.range(0, n).mapToObj(i -> new ArrayList<Integer>()).collect(Collectors.toList());
