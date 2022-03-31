2de0dc3bca67627d647629b51154ee4667da98bb
igor <climberig@gmail.com>
2022-03-30 20:56:17 -0700

    new solutions

--- src/leetcode/p14.java
+++ src/leetcode/p14.java
@@ -151,13 +151,11 @@ public class p14{
     }
 
     static class s1458{//Max Dot Product of Two Subsequences
-        public int maxDotProduct(int[] a, int[] b){
-            int n = a.length, m = b.length, dp[][] = new int[n][m];
+        public int maxDotProduct(int[] a1, int[] a2){
+            int n = a1.length, m = a2.length, dp[][] = new int[n][m];
             for(int i = 0; i < n; i++)
                 for(int j = 0; j < m; j++){
-                    dp[i][j] = a[i] * b[j];
-                    if(i > 0 && j > 0)
-                        dp[i][j] += Math.max(dp[i - 1][j - 1], 0);
+                    dp[i][j] = a1[i] * a2[j] + (i > 0 && j > 0 ? Math.max(dp[i - 1][j - 1], 0) : 0);
                     if(i > 0)
                         dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
                     if(j > 0)
