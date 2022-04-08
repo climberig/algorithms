c7d15eb0878f71cc5ec31e88bd25208c32417c2b
igor <climberig@gmail.com>
2022-04-07 17:17:10 -0700

    new solutions

--- src/leetcode/a.java
+++ src/leetcode/a.java
@@ -1,24 +1,5 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
 public class a{
-    static class s703{//Kth Largest Element in a Stream
-        class KthLargest{
-            PriorityQueue<Integer> q = new PriorityQueue<>();
-            int k;
 
-            public KthLargest(int k, int[] a){
-                this.k = k;
-                Arrays.stream(a).forEach(this::add);
-            }
-
-            public int add(int val){
-                q.offer(val);
-                if(q.size() > k)
-                    q.poll();
-                return q.peek();
-            }
-        }
-    }
 }
--- src/leetcode/p7.java
+++ src/leetcode/p7.java
@@ -12,6 +12,25 @@ public class p7{
         }
     }
 
+    static class s703{//Kth Largest Element in a Stream
+        class KthLargest{
+            PriorityQueue<Integer> q = new PriorityQueue<>();
+            int k;
+
+            public KthLargest(int k, int[] a){
+                this.k = k;
+                Arrays.stream(a).forEach(this::add);
+            }
+
+            public int add(int val){
+                q.offer(val);
+                if(q.size() > k)
+                    q.poll();
+                return q.peek();
+            }
+        }
+    }
+
     static class s704{//Binary Search
         public int search(int[] a, int target){
             int lo = 0, hi = a.length - 1;
