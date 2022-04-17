01c9f26d239532da8849f55a0a26ebbe9af5d1e3
igor <climberig@gmail.com>
2022-04-16 21:48:43 -0700

    new solutions

--- src/leetcode/p22.java
+++ src/leetcode/p22.java
@@ -542,4 +542,20 @@ public class p22{
                 q.poll();
         }
     }
+
+    static class s2243{//Calculate Digit Sum of a String
+        public String digitSum(String s, int k){
+            if(s.length() <= k)
+                return s;
+            StringBuilder r = new StringBuilder();
+            for(int i = 1, sum = 0; i <= s.length(); i++){
+                sum += s.charAt(i - 1) - '0';
+                if(i % k == 0 || i == s.length()){
+                    r.append(sum);
+                    sum = 0;
+                }
+            }
+            return digitSum(r.toString(), k);
+        }
+    }
 }
