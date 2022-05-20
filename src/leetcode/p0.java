ce047a141ad9dfb3005f2e2c985881be301f381b
igor <climberig@gmail.com>
2022-05-19 18:14:28 -0700

    new solutions

--- src/leetcode/p0.java
+++ src/leetcode/p0.java
@@ -171,6 +171,21 @@ public class p0{
         }
     }
 
+    static class s63{//Unique Paths II
+        public int uniquePathsWithObstacles(int[][] g){
+            int[][] dp = new int[g.length][g[0].length];
+            dp[0][0] = 1 - g[0][0];
+            for(int i = 1; i < g[0].length; i++)
+                dp[0][i] = g[0][i] == 1 ? 0 : dp[0][i - 1];
+            for(int i = 1; i < g.length; i++)
+                dp[i][0] = g[i][0] == 1 ? 0 : dp[i - 1][0];
+            for(int i = 1; i < g.length; i++)
+                for(int j = 1; j < g[0].length; j++)
+                    dp[i][j] = g[i][j] == 1 ? 0 : dp[i - 1][j] + dp[i][j - 1];
+            return dp[g.length - 1][g[0].length - 1];
+        }
+    }
+
     static class s69{//Sqrt(x)
         public int mySqrt(int x){
             if(x <= 1)
