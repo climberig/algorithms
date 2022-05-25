fc2389884ac1231d054c3f3a44c5b46eb57383ed
igor <climberig@gmail.com>
2022-05-24 17:09:05 -0700

    new solutions

--- src/leetcode/p3.java
+++ src/leetcode/p3.java
@@ -355,6 +355,22 @@ public class p3{
         }
     }
 
+    static class s354{//Russian Doll Envelopes
+        public int maxEnvelopes(int[][] envelopes){
+            Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+            int dp[] = new int[envelopes.length], r = 0;
+            for(int[] e : envelopes){
+                int i = Arrays.binarySearch(dp, 0, r, e[1]);
+                if(i < 0)
+                    i = -(i + 1);
+                dp[i] = e[1];
+                if(i == r)
+                    r++;
+            }
+            return r;
+        }
+    }
+
     static class s359{//Logger Rate Limiter
         class Logger{
             Map<String, Integer> m = new HashMap<>();
