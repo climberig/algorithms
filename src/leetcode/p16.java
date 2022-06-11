878fafb0a9b19a4d9d307d0874787d8510ac07bd
igor <climberig@gmail.com>
2022-06-10 17:43:52 -0700

    new problems

--- src/leetcode/p16.java
+++ src/leetcode/p16.java
@@ -226,6 +226,23 @@ public class p16{
         }
     }
 
+    static class s1658{//Minimum Operations to Reduce X to Zero
+        public int minOperations(int[] a, int x){
+            int target = Arrays.stream(a).sum() - x, sum = 0, r = Integer.MIN_VALUE;
+            if(target == 0)
+                return a.length;
+            Map<Integer, Integer> m = new HashMap<>();
+            m.put(0, -1);
+            for(int i = 0; i < a.length; i++){
+                sum += a[i];
+                if(m.containsKey(sum - target))
+                    r = Math.max(r, i - m.get(sum - target));
+                m.put(sum, i);
+            }
+            return r == Integer.MIN_VALUE ? -1 : a.length - r;
+        }
+    }
+
     static class s1664{//Ways to Make a Fair Array
         public int waysToMakeFair(int[] a){
             int r = 0, L[] = new int[2], R[] = new int[2];
