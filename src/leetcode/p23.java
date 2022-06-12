d5ead2ff7d67521ccb4d92d49af9b79fc219c2ab
igor <climberig@gmail.com>
2022-06-11 21:34:40 -0700

    new problems

--- src/leetcode/p23.java
+++ src/leetcode/p23.java
@@ -1,3 +1,14 @@
 package leetcode;
 public class p23{
+    static class s2303{//Calculate Amount Paid in Taxes
+        public double calculateTax(int[][] brackets, int income){
+            double r = 0;
+            for(int i = 0, amount = 0; i < brackets.length && amount < income; i++){
+                int dollars = Math.min(income - amount, brackets[i][0] - amount);
+                r += dollars * brackets[i][1] / 100.0;
+                amount += dollars;
+            }
+            return r;
+        }
+    }
 }
