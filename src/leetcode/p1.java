aa666e271f9f49b434279de98b83ce8cbba21746
igor <climberig@gmail.com>
2022-06-12 17:19:54 -0700

    new problems

--- src/leetcode/p1.java
+++ src/leetcode/p1.java
@@ -56,6 +56,15 @@ public class p1{
         }
     }
 
+    static class s120{//Triangle
+        public int minimumTotal(List<List<Integer>> t){
+            for(int i = t.size() - 2; i >= 0; i--)
+                for(int j = 0; j < t.get(i).size(); j++)
+                    t.get(i).set(j, t.get(i).get(j) + Math.min(t.get(i + 1).get(j), t.get(i + 1).get(j + 1)));
+            return t.get(0).get(0);
+        }
+    }
+
     static class s121{//Best Time to Buy and Sell Stock
         public int maxProfit(int[] prices){
             int r = 0, min = prices[0];
