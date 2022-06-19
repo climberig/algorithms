f1d4201694f84610b49fb8cd232cbb62bbe808ff
igor <climberig@gmail.com>
2022-06-18 17:51:39 -0700

    new problems

--- src/leetcode/p12.java
+++ src/leetcode/p12.java
@@ -467,6 +467,35 @@ public class p12{
         }
     }
 
+    static class s1268{//Search Suggestions System
+        public List<List<String>> suggestedProducts(String[] products, String searchWord){
+            Trie trie = new Trie();
+            for(String product : products){
+                Trie node = trie;
+                for(char c : product.toCharArray()){
+                    if(node.children[c - 'a'] == null)
+                        node.children[c - 'a'] = new Trie();
+                    node = node.children[c - 'a'];
+                    node.suggestions.add(product);
+                    if(node.suggestions.size() > 3)
+                        node.suggestions.pollLast();
+                }
+            }
+            List<List<String>> r = new ArrayList<>(searchWord.length());
+            for(char c : searchWord.toCharArray()){
+                if(trie != null)
+                    trie = trie.children[c - 'a'];
+                r.add(trie != null ? new ArrayList<>(trie.suggestions) : Collections.emptyList());
+            }
+            return r;
+        }
+
+        class Trie{
+            TreeSet<String> suggestions = new TreeSet<>();
+            Trie[] children = new Trie[26];
+        }
+    }
+
     static class s1271{//Hexspeak
         public String toHexspeak(String s){
             char[] hex = Long.toHexString(Long.parseLong(s)).toCharArray();
