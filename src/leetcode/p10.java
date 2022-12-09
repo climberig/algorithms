cbbf0569c62069615f1eeee780a6b579e4c0447c
igor <climberig@gmail.com>
2022-12-08 22:05:19 -0800

    new problems

--- src/leetcode/p10.java
+++ src/leetcode/p10.java
@@ -99,6 +99,20 @@ public class p10{
         }
     }
 
+    static class s1026{//Maximum Difference Between Node and Ancestor
+        public int maxAncestorDiff(TreeNode root) {
+            return maxDiff(root, root.val, root.val);
+        }
+
+        int maxDiff(TreeNode node, int min, int max) {
+            if (node == null)
+                return max - min;
+            min = Math.min(min, node.val);
+            max = Math.max(max, node.val);
+            return Math.max(maxDiff(node.left, min, max), maxDiff(node.right, min, max));
+        }
+    }
+
     static class s1031{//Maximum Sum of Two Non-Overlapping Subarrays
         public int maxSumTwoNoOverlap(int[] a, int L, int R){
             int[] cs = new int[a.length + 1];
