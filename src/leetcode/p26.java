ccb8a651c5941f30c0dc308d07521d1b0ce1b0ac
igor <climberig@gmail.com>
2023-03-25 21:54:17 -0700

    new problems

--- src/leetcode/p26.java
+++ src/leetcode/p26.java
@@ -1,3 +1,8 @@
 package leetcode;
 public class p26{
+    static class s2600{//K Items With the Maximum Sum
+        public int kItemsWithMaximumSum(int nOnes, int nZero, int nNegOnes, int k) {
+            return Math.min(nOnes, k) - Math.max(0, k - nOnes - nZero);
+        }
+    }
 }
