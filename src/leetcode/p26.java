2a843c27e423a19778276d92e801bc52641d9267
igor <climberig@gmail.com>
2023-03-26 19:18:38 -0700

    new problems

--- src/leetcode/p26.java
+++ src/leetcode/p26.java
@@ -5,4 +5,26 @@ public class p26{
             return Math.min(nOnes, k) - Math.max(0, k - nOnes - nZero);
         }
     }
+
+    static class s2601{//Prime Subtraction Operation
+        public boolean primeSubOperation(int[] a) {
+            int prev = 0;
+            for (int n : a) {
+                int maxP = 0;
+                for (int p = 2; p < n; p++)
+                    if (n - p > prev && prime(p))
+                        maxP = p;
+                if (prev >= n - maxP)
+                    return false;
+                prev = n - maxP;
+            }
+            return true;
+        }
+        boolean prime(int p) {
+            for (int d = 2; d <= Math.sqrt(p); d++)
+                if (p % d == 0)
+                    return false;
+            return true;
+        }
+    }
 }
