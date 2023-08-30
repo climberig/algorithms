d910cbda5446402861a16bf4de5ff3dcb31c8308
igor <climberig@gmail.com>
2023-08-30 13:40:50 -0700

    new problems

--- src/leetcode/p28.java
+++ src/leetcode/p28.java
@@ -1,7 +1,19 @@
 package leetcode;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 public class p28{
+    static class s2810{//Faulty Keyboard
+        public String finalString(String s) {
+            StringBuilder r = new StringBuilder();
+            for (char c : s.toCharArray())
+                if (c == 'i')
+                    r.reverse();
+                else r.append(c);
+            return r.toString();
+        }
+    }
+
     static class s2824{//Count Pairs Whose Sum is Less than Target
         public int countPairs(List<Integer> a, int target) {
             Collections.sort(a);
@@ -11,6 +23,12 @@ public class p28{
                 r += Math.min(i, j);
             }
             return r;
+        }
+    }
+
+    static class s2828{//Check if a String Is an Acronym of Words
+        public boolean isAcronym(List<String> words, String s) {
+            return words.stream().map(w -> w.charAt(0) + "").collect(Collectors.joining("")).equals(s);
         }
     }
 }
