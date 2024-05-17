041abe4456a44dc1c6df4a4d5727e9c0778815aa
igor <climberig@gmail.com>
2024-05-17 10:57:09 -0700

    new problems

--- src/leetcode/p31.java
+++ src/leetcode/p31.java
@@ -1,3 +1,13 @@
 package leetcode;
 public class p31{
+    static class s3146{//Permutation Difference between Two Strings
+        public int findPermutationDifference(String s, String t) {
+            int p[] = new int[26], r = 0;
+            for (int i = 0; i < s.length(); i++)
+                p[s.charAt(i) - 'a'] = i;
+            for (int i = 0; i < t.length(); i++)
+                r += Math.abs(i - p[t.charAt(i) - 'a']);
+            return r;
+        }
+    }
 }
