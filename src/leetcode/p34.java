3e64512131e9d06510ad1f1633251d4fa782b92c
igor <climberig@gmail.com>
2025-01-07 08:59:44 -0800

    new problems

--- src/leetcode/p34.java
+++ src/leetcode/p34.java
@@ -1,5 +1,19 @@
 package leetcode;
 public class p34{
+    static class s3402{//Minimum Operations to Make Columns Strictly Increasing
+        public int minimumOperations(int[][] g) {
+            int r = 0;
+            for (int col = 0; col < g[0].length; col++) {
+                int val = g[0][col];
+                for (int row = 1; row < g.length; row++) {
+                    val = Math.max(g[row][col], val + 1);
+                    r += val - g[row][col];
+                }
+            }
+            return r;
+        }
+    }
+
     static class s3407{//Substring Matching Pattern
         public boolean hasMatch(String s, String p) {
             int k = p.indexOf('*');
