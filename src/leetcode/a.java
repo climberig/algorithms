f028a972ba6a6e9c80c2eeb40e7195e9f5ce123a
igor <climberig@gmail.com>
2025-12-08 14:23:50 -0800

    new solution

--- src/leetcode/a.java
+++ src/leetcode/a.java
@@ -1,5 +1,5 @@
 package leetcode;
 
-public class a {
+public class a{
 
 }
--- src/leetcode/p37.java
+++ src/leetcode/p37.java
@@ -1,6 +1,15 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class p37 {
+    static class s3745{//Maximize Expression of Three Elements
+        public int maximizeExpressionOfThree(int[] a) {
+            Arrays.sort(a);
+            return a[a.length - 1] + a[a.length - 2] - a[0];
+        }
+    }
+
     static class s3754 {//Concatenate Non-Zero Digits and Multiply by Sum I
         public long sumAndMultiply(int n) {
             int x = 0, sum = 0;
