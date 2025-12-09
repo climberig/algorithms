1ce1fd729cb160c819dc26aef82ff30616e4289d
igor <climberig@gmail.com>
2025-12-08 18:38:40 -0800

    new solution

--- src/leetcode/p37.java
+++ src/leetcode/p37.java
@@ -1,8 +1,23 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class p37 {
+    static class s3731{//Find Missing Elements
+        public List<Integer> findMissingElements(int[] a){
+            TreeSet<Integer> s = Arrays.stream(a).boxed().collect(Collectors.toCollection(TreeSet::new));
+            List<Integer> r = new ArrayList<>();
+            for(int i = s.first() + 1; i < s.last(); i++)
+                if(!s.contains(i))
+                    r.add(i);
+            return r;
+        }
+    }
+
     static class s3745{//Maximize Expression of Three Elements
         public int maximizeExpressionOfThree(int[] a) {
             Arrays.sort(a);
