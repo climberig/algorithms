d5d087d6041db6fb2c7b00e308199b71b2214f75
igor <climberig@gmail.com>
2026-01-22 11:03:02 -0800

    new solutions

--- src/leetcode/a.java
+++ src/leetcode/a.java
@@ -1,21 +1,5 @@
 package leetcode;
 
-import java.util.Set;
-public class a {
-    static class ss3813 {//Vowel-Consonant Score
-        public int vowelConsonantScore(String s) {
-            int c = 0, v = 0;
-            for (char ch : s.toCharArray())
-                if (Character.isLetter(ch)) {
-                    if (isVowel(ch))
-                        v++;
-                    else c++;
-                }
-            return c == 0 ? 0 : Math.floorDiv(v, c);
-        }
+public class a{
 
-        boolean isVowel(char c) {
-            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-        }
-    }
 }
