3d62a2b0e79ae4fc911db2f2e6c78b59c4438a1c
igor <climberig@gmail.com>
2026-01-22 18:52:55 -0800

    new solutions

--- src/leetcode/p38.java
+++ src/leetcode/p38.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
 public class p38 {
     static class s3803 {//Count Residue Prefixes
         public int residuePrefixes(String s) {
@@ -8,6 +10,23 @@ public class p38 {
                 b |= 1 << (s.charAt(i) - 'a');
                 if (Integer.bitCount(b) == ((i + 1) % 3))
                     r++;
+            }
+            return r;
+        }
+    }
+
+    static class s3804 {//Number of Centered Subarrays
+        public int centeredSubarrays(int[] a) {
+            int r = 0;
+            for (int i = 0; i < a.length; i++) {
+                int sum = 0;
+                Set<Integer> s = new HashSet<>();
+                for (int j = i; j < a.length; j++) {
+                    sum += a[j];
+                    s.add(a[j]);
+                    if (s.contains(sum))
+                        r++;
+                }
             }
             return r;
         }
